040b0f0da118e4f8eebe2bee9c51dbdd770ae461
Oscar Huber (DanMX8) <odahuber-22@hotmail.com>
2023-11-13 23:22:25 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySurveyApp.Models;
+using System.Diagnostics;
+
+namespace MySurveyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Blazor()
+        {
+            return View("_Host");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("fetchdata")]
+        public IActionResult FetchData()
+        {
+            return View();
+        }
+        [Route("counter")]
+        public IActionResult Counter()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
